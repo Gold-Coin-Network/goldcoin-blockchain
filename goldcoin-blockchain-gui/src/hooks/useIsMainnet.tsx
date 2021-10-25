9c6af540b582f79c0c850e45033fc311d75e5541
Gold-Coin-Network <90006891+Gold-Coin-Network@users.noreply.github.com>
2021-10-25 02:12:16 +0200

    Update useIsMainnet.tsx

--- goldcoin-blockchain-gui/src/hooks/useIsMainnet.tsx
+++ goldcoin-blockchain-gui/src/hooks/useIsMainnet.tsx
@@ -10,5 +10,5 @@ export default function useIsMainnet(): boolean | undefined {
     return undefined;
   }
 
-  return networkPrefix.toLowerCase() === 'stai';
+  return networkPrefix.toLowerCase() === 'ozt';
 }
